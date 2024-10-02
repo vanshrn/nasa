c590ef2b7121411a9e399ae0de1b3ab59400d835
Digant07 <155979245+Digant07@users.noreply.github.com>
2024-10-02 20:26:04 +0530

    Initial commit

---
+++ src/spline-viewer/Earth.jsx
@@ -0,0 +1,12 @@
+function Earth() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <spline-viewer url="https://prod.spline.design/Xtha61Vv-snDhRXz/scene.splinecode"></spline-viewer>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Earth;
+  
