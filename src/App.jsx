ec159c74a9ed48e5d9fb57818462d7b21bb6ac1b
Digant07 <155979245+Digant07@users.noreply.github.com>
2024-10-03 23:38:36 +0530

    Some more improvement

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import Navbar from "./compenents/Navbar";
+import Navbar from "./compenents/Navbar/Navbar";
 import Earth from "./spline-viewer/Earth";
+
 import "./App.css";
 import { Fragment } from "react";
 
@@ -8,29 +9,23 @@ function App() {
     <Fragment>
       <Earth></Earth>
       <Navbar></Navbar>
-      <div class="header">
-        <h1>ASTEROID TRACKER</h1>
-      </div>
-      <div class="search-section">
-        <div class="form">
-          <input placeholder="Enter Asteroid Name" type="text" />
-          <input placeholder="Enter Asteroid Size" type="range" />
-          <input placeholder="Enter Asteroid Distance" type="text" />
-          <input placeholder="Enter Asteroid Direction" type="text" />
-          <button>SEARCH</button>
-        </div>
-        <div class="visual">
-          <img
-            alt="Asteroid visualization with orbit paths and labels for name, size, and distance"
-            height="400"
-            src="https://storage.googleapis.com/a1aa/image/odzQVSD7AUamMRnZB0AsTYvezYfbkbIgnLeKtOpsfiLDllKOB.jpg"
-            width="600"
-          />
+      
+      <div className="search-section">
+          <div className="main"></div>
+          <div className="form">
+            <input placeholder="Enter Asteroid Name" type="text" style={{ color: 'black' }} />
+            <input placeholder="Enter Asteroid Size (In KM )" type="number" style={{ color: 'black' }} />
+            <input placeholder="Enter Asteroid Distance ( In AU )" type="number" style={{ color: 'black' }} />
+            <input placeholder="Enter Ecliptic Longitude (λ) " type="text" style={{ color: 'black' }} />
+            <button type="submit">SEARCH</button>
+          </div>
+        <div className="header">
+          <h1>ASTEROID TRACKER</h1>
         </div>
       </div>
-      <div class="cards">
-        <div class="card">
-          <h2>NAME</h2>
+      <div className="cards">
+        <div className="card">
+          <h2> NAME </h2>
           <img
             alt="Asteroid image with orbit paths"
             height="500"
@@ -41,8 +36,8 @@ function App() {
           <p>Distance: 5678 km</p>
           <p>Direction: North</p>
         </div>
-        <div class="card">
-          <h2>SIZE</h2>
+        <div className="card">
+          <h2> SIZE </h2>
           <img
             alt="Asteroid image with orbit paths"
             height="300"
@@ -53,8 +48,8 @@ function App() {
           <p>Distance: 5678 km</p>
           <p>Direction: North</p>
         </div>
-        <div class="card">
-          <h2>DIRECTION</h2>
+        <div className="card">
+          <h2> DIRECTION </h2>
           <img
             alt="Asteroid image with orbit paths"
             height="300"
